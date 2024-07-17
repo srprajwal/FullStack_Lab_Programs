53d9713b282c7be094e9a7c82d5e41ad608ef68a
srpraju <srpraju1554@gmail.com>
2024-07-17 21:11:14 +0530

    pro10

---
+++ pro10/pro10/pro10_app/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Student, Course,Project
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['first_name', 'last_name', 'email', 'courses', 'projects']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'courses': forms.SelectMultiple(attrs={'class': 'form-select', 'data-toggle':'dropdown'}),
+            'projects': forms.SelectMultiple(attrs={'class': 'form-select', 'data-toggle':'dropdown'}),
+        }
+
+class CourseForm(forms.ModelForm):
+  class Meta:
+    model = Course
+    fields = ['name', 'description']
+    widgets = {
+      'name': forms.TextInput(attrs={'class': 'form-control'}),
+      'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+    }
+
+class ProjectForm(forms.ModelForm):
+  class Meta:
+    model = Project
+    fields = ('topic', 'languages_used', 'duration')
+    widgets = {
+      'topic': forms.TextInput(attrs={'class': 'form-control'}),
+      'languages_used': forms.TextInput(attrs={'class': 'form-control'}),
+      'duration': forms.TextInput(attrs={'class': 'form-control'}),
+      }
---
+++ pro10/pro10/pro10_app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course,Project,Student
+from .forms import StudentForm, CourseForm,ProjectForm
+from django.views.generic import ListView, DetailView
+
+def index(request):
+  courses = Course.objects.all()
+  projects = Project.objects.all()
+  students = Student.objects.all()
+  return render(request, 'index.html', {'courses': courses,'projects': projects,'students':students})
+
+def register_student(request):
+  if request.method == 'POST':
+    form = StudentForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('index')
+  else:
+    form = StudentForm()
+  return render(request, 'register_student.html', {'form': form})
+
+def register_course(request):
+  if request.method == 'POST':
+    form = CourseForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('index')
+  else:
+    form = CourseForm()
+  return render(request, 'register_course.html', {'form': form})
+
+def student_list(request, course_id):
+  course = Course.objects.get(id=course_id)
+  students = course.students.all()
+  return render(request, 'student_list.html', {'students': students, 'course': course})
+
+def student_detail(request, stu_id):
+    student = get_object_or_404(Student, id=stu_id)
+    return render(request, 'student_detail.html', {'student': student})
+
+def project_details(request, project_id):
+  project = get_object_or_404(Project, pk = project_id)
+  return render(request, 'project_details.html', {'project': project})
+
+def project_student_list(request, project_id):
+  project = Project.objects.get(id=project_id)
+  students = project.students.all()
+  return render(request, 'project_student_list.html', {'students': students, 'project': project})
+
+def register_project(request):
+  if request.method == 'POST':
+    form = ProjectForm(request.POST)
+    if form.is_valid():
+      project = form.save()
+      return redirect('index')
+      #return redirect('project_detail', project_id=project.pk)
+  else:
+    form = ProjectForm()
+  return render(request, 'register_project.html', {'form': form})
+
+
+
+class StudentListView(ListView):
+  model = Student
+  template_name = 'stu_list.html'
+  context_object_name = 'students'
+
+class StudentDetailView(DetailView):
+  model = Student
+  template_name = 'student_detail.html'
+  context_object_name = 'student'
